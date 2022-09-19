eefa733392ff418b73b0ef97dfcfaa43ef53920c
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-09-19 15:48:51 -0400

    adding read of input file

--- MergeSort.java
+++ MergeSort.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 class MergeSort
 {
     // Merges two subarrays of arr[].
@@ -81,7 +87,38 @@ class MergeSort
 
     // Driver code
     public static void main(String args[])
+            throws IOException
     {
+
+        List<String> listOfStrings
+                = new ArrayList<String>();
+
+        // load data from file
+        BufferedReader bf = new BufferedReader(
+                new FileReader("input.txt"));
+
+        // read entire line as string
+        String line = bf.readLine();
+
+        // checking for end of file
+        while (line != null) {
+            listOfStrings.add(line);
+            line = bf.readLine();
+        }
+
+        // closing bufferreader object
+        bf.close();
+
+        // storing the data in arraylist to array
+        String[] array
+                = listOfStrings.toArray(new String[0]);
+
+        // printing each line of file
+        // which is stored in array
+        for (String str : array) {
+            System.out.println(str);
+        }
+
         int arr[] = { 12, 11, 13, 5, 6, 7 };
 
         System.out.println("Given Array");
