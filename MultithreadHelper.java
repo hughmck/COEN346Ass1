a273b76da67ac0ec09901e64cca87060273fc2de
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-09-24 18:21:31 -0400

    WIP

--- MultithreadHelper.java
+++ MultithreadHelper.java
@@ -4,7 +4,6 @@ public class MultithreadHelper extends Thread{
     public void run(){
         for(int i =1; i<= 5; i++){
             System.out.println("thread test");
-
             try {
                 Thread.sleep(1000);}
             catch (InterruptedException e) {
