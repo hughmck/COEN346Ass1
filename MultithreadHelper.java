743d451759ff8544ccbd3948d893d27704143fb2
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-09-24 18:37:11 -0400

    WIP

--- MultithreadHelper.java
+++ MultithreadHelper.java
@@ -1,7 +1,13 @@
+
 public class MultithreadHelper extends Thread{
 
     @Override
     public void run(){
+
+        MergeSort getArray = new MergeSort();
+
+        int[] multiThreadArray = getArray.getNumArray(); //trying to take the array from the MergeSort file into here, so we can use a loop and initilize each thread
+
         for(int i =1; i<= 5; i++){
             System.out.println("thread test");
             try {
