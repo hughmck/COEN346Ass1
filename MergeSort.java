43f0057bfe86526f2343feb031041d7ae0b5ae02
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-09-24 17:20:03 -0400

    WIP

--- MergeSort.java
+++ MergeSort.java
@@ -7,31 +7,21 @@ import java.util.Arrays;
 
 class MergeSort
 {
-    // Merges two subarrays of arr[].
-    // First subarray is arr[l..m]
-    // Second subarray is arr[m+1..r]
     void merge(int arr[], int l, int m, int r)
     {
-        // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        /* Create temp arrays */
         int L[] = new int[n1];
         int R[] = new int[n2];
 
-        /*Copy data to temp arrays*/
         for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
 
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
         int i = 0, j = 0;
 
-        // Initial index of merged subarray array
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
@@ -45,14 +35,12 @@ class MergeSort
             k++;
         }
 
-        /* Copy remaining elements of L[] if any */
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
 
-        /* Copy remaining elements of R[] if any */
         while (j < n2) {
             arr[k] = R[j];
             j++;
@@ -60,8 +48,6 @@ class MergeSort
         }
     }
 
-    // Main function that sorts arr[l..r] using
-    // merge()
     void sort(int arr[], int l, int r)
     {
         if (l < r) {
@@ -120,19 +106,8 @@ class MergeSort
             System.out.println(str);
         }
 
-        int arr[] = { 12, 11, 13, 5, 6, 7 };
-
-        System.out.println("Given Array");
-        printArray(arr);
-
-        MergeSort ob = new MergeSort();
-        ob.sort(arr, 0, arr.length - 1);
-
-        System.out.println("\nSorted array");
-        printArray(arr);
-
         for (int i = 0; i < array.length; i++) {
-            arr[i] = Integer.valueOf(array[i]);
+            array[i] = Integer.valueOf(array[i]);
         }
 
         // printing string
@@ -141,6 +116,16 @@ class MergeSort
         // printing the Integer array
         System.out.print("\nInteger array : "
                 + Arrays.toString(array));
+
+
+        System.out.println("Given Array");
+        printArray(array);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(array, 0, array.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(array);
     }
 }
 
