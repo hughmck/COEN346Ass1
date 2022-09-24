97045f139a84ed21783d9251dea31533ecb20f3c
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-09-24 18:04:19 -0400

    creating multiple threads test

---
+++ MultithreadHelper.java
@@ -0,0 +1,14 @@
+public class MultithreadHelper extends Thread{
+
+    @Override
+    public void run(){
+        for(int i =1; i<= 5; i++){
+            System.out.println("thread test");
+
+            try {
+                Thread.sleep(1000);}
+            catch (InterruptedException e) {
+            }
+        }
+    }
+}
