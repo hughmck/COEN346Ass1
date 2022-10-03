6bf6b8dcf26b953e866c72f965ee8a15566158be
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-10-03 11:07:01 -0400

    changing file

--- MultithreadHelper.java
+++ MultithreadHelper.java
@@ -4,14 +4,6 @@ public class MultithreadHelper extends Thread{
     @Override
     public void run(){
 
-        MergeSort getArray = new MergeSort();
 
-        for(int i =1; i<= 5; i++){
-            System.out.println("thread test");
-            try {
-                Thread.sleep(1000);}
-            catch (InterruptedException e) {
-            }
-        }
     }
 }
